15c86bc90bab7fa4c407c5fcaae939437c88a9c4
Sarthak manral <sarthakmanral19@gmail.com>
2025-09-04 13:26:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'   // importing styles for this component
+
+function App() {
+  return (
+    <>
+      <h1>lets built gmail clone </h1>
+    </>
+  )
+}
+
+export default App   // exporting App so main.jsx can render it
